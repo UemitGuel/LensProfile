3f082844a26710f79b759d5647782ef9b4ee4fe2
Ümit Gül <uemitgul@gmail.com>
2022-09-16 21:14:30 +0200

    Initial Commit

---
+++ LensProfile/LensProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  LensProfileApp.swift
+//  LensProfile
+//
+//  Created by Ümit Gül on 16.09.22.
+//
+
+import SwiftUI
+
+@main
+struct LensProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
